414b65830106ba5ba2144ee8ac5626145fb09742
figurantpp <figurantpp@protonmail.com>
2023-01-31 18:48:45 -0300

    Initial Commit

---
+++ project.el
@@ -0,0 +1,42 @@
+;;; project.el --- Dungeons and Dragons 2023 Session Reference -*- lexical-binding: t; -*-
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary: :v
+;;
+;;  Description
+;;
+;;; Code:
+
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(
+        ("org-notes"
+         :base-directory "~/tmp/dnd/"
+         :base-extension "org"
+         :publishing-directory "~/tmp/dnd/public"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+
+         :with-toc nil
+         :with-author nil
+         )
+
+        ("org-static"
+         :base-directory "~/tmp/dnd/"
+         :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+         :publishing-directory "~/tmp/dnd/public/"
+         :recursive t
+         :publishing-function org-publish-attachment
+         )
+
+        ("org" :components ("org-notes" "org-static"))
+
+        ))
+
+
+(provide 'project)
+
+;;; project.el ends here
